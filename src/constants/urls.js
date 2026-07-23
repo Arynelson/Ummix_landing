ad6ff6fc59a8519c5b51be8a268e1a16bbdae44b
Ary <aryhauffe@gmail.com>
2026-07-23 13:56:03 +0200

    23_7

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -2,4 +2,5 @@
 export const PLATFORM_SIGNUP = 'https://app.ummix.com.br'
 export const PLATFORM_LOGIN  = 'https://app.ummix.com.br'
 export const CONTACT_EMAIL = 'falecoma@ummix.com.br'
-export const CONTACT_FORM_ENDPOINT = `https://formsubmit.co/ajax/${CONTACT_EMAIL}`
+// FormSubmit's private endpoint token, generated during recipient activation.
+export const CONTACT_FORM_ENDPOINT = 'https://formsubmit.co/ajax/falecoma@ummix.com.br'
